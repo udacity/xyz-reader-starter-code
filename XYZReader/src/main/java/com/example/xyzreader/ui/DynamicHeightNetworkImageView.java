132bcbdc039160f5c78a38ead55f9a5b181db0f4
Rokas Leskevicius <rokaslopas@gmail.com>
2017-10-15 00:00:30 -0400

    It looks better already

--- XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -28,7 +28,7 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int measuredWidth = getMeasuredWidth();
-        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+//        int measuredWidth = getMeasuredWidth();
+//        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
     }
 }
